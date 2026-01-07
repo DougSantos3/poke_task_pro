c142623fcc9c2189443d9ddf205302e3c4e9fb4c
Doug Santos <douglasmartinssantos7@gmail.com>
2026-01-07 17:38:33 -0300

    Add TodoRepositoryImpl class for task management functionality

---
+++ lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:poke_task_pro/features/todo/data/datasources/pokemon_remote_datasource.dart';
+import 'package:poke_task_pro/features/todo/domain/entities/task.dart';
+import 'package:poke_task_pro/features/todo/domain/repositories/todo_repository.dart';
+
+class TodoRepositoryImpl implements  TodoRepository {
+  final PokemonRemoteDataSource remoteDataSource;
+
+  TodoRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<void> addTask(Task task) async {
+    // Aqui você implementaria a lógica de salvar na sua API Java futura
+    // Por enquanto, podemos simular um print
+    print('Adding task: ${task.title} with Pokémon id ${task.pokemon?.name}');
+  }
+
+}

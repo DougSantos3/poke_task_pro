c37b0301538e30149029c3cef1739c37d02ccbfc
Doug Santos <douglasmartinssantos7@gmail.com>
2026-01-14 10:24:06 -0300

    chore: add more test in pokemon_remote_datasource

--- test/features/todo/data/datasources/pokemon_remote_datasource_test.dart
+++ test/features/todo/data/datasources/pokemon_remote_datasource_test.dart
@@ -2,11 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:http/http.dart' as http;
 import 'package:poke_task_pro/features/todo/data/datasources/pokemon_remote_datasource.dart';
+import 'package:poke_task_pro/features/todo/data/models/pokemon_model.dart';
 import 'dart:io';
+import '../../../../fixtures/fixture_reader.dart';
 
 class MockHttpClient extends Mock implements http.Client {}
-
-String fixture(String name) => File('test/fixtures/$name').readAsStringSync();
 
 void main() {
   late PokemonRemoteDataSourceImpl dataSource;
@@ -18,16 +18,44 @@ void main() {
     registerFallbackValue(Uri());
   });
 
-  test('Deve realizar uma chamada Get na Url correta com o Id do pokemon',
-      () async {
-    // Arrange
-    when(() => mockHttpClient.get(any())).thenAnswer(
-      (_) async => http.Response(fixture('pokemon_fixture.json'), 200),
-    );
-    // Act
-    await dataSource.getPokemon(1);
-    // Assert
-    verify(() =>
-        mockHttpClient.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/1')));
+  /*Aqui testamos se o DataSource chama a URL correta e se ele lança uma 
+  exceção caso a internet falhe (Erro 404 ou 500) */
+  group('getPokemon', () {
+    test('Deve realizar uma chamada Get na Url correta com o Id do pokemon',
+        () async {
+      // Arrange
+      when(() => mockHttpClient.get(any())).thenAnswer(
+        (_) async => http.Response(fixture('pokemon_fixture.json'), 200),
+      );
+      // Act
+      await dataSource.getPokemon(1);
+      // Assert
+      verify(() =>
+          mockHttpClient.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/1')));
+    });
+
+    test('Deve retornar PokemonModel quando a reposta for 200(sucesso)',
+        () async {
+      // Arrange
+      when(() => mockHttpClient.get(any())).thenAnswer(
+        (_) async => http.Response(fixture('Pokemon_fixture.json'), 200),
+      );
+      // Act
+      final result = await dataSource.getPokemon(1);
+      // Assert
+      expect(result, isA<PokemonModel>());
+    });
+
+    test('Deve lançar uma Exception quando a resposta for 404 (Erro)', () {
+      // Arrange
+      when(()=> mockHttpClient.get(any())).thenAnswer(
+        (_) async => http.Response('Algo deu errado', 404)
+      );
+
+      // Act
+      final call = dataSource.getPokemon;
+      // Assert 
+      expect(() => call(1), throwsException)
+    });
   });
 }

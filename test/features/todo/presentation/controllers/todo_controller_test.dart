0c5efbf43528d480153e95114fb36ef832dc054f
Doug Santos <douglasmartinssantos7@gmail.com>
2026-01-14 11:39:47 -0300

    chore: add import, task and ; in the last

--- test/features/todo/presentation/controllers/todo_controller_test.dart
+++ test/features/todo/presentation/controllers/todo_controller_test.dart
@@ -1,12 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:poke_task_pro/features/todo/domain/entities/task.dart';
+import 'package:poke_task_pro/features/todo/domain/entities/task_status.dart';
+import 'package:poke_task_pro/features/todo/presentation/controllers/todo_controller.dart';
+import 'package:poke_task_pro/features/todo/domain/usecases/add_task.dart';
 
+class MockAddTask extends Mock implements AddTask {}
 
-class MockAddTask implements AddTask {}
+class FakeTask extends Fake implements Task {}
 
 void main() {
-  test('Deve adicionar uma nova task à lista quando o usecase for bem sucedido', () async {
+  setUpAll(() {
+    registerFallbackValue(FakeTask());
+  });
+
+  test('Deve adicionar uma nova task à lista quando o usecase for bem sucedido',
+      () async {
     final mockUseCase = MockAddTask();
     final controller = TodoController(mockUseCase);
-    final tTask = Task(/* dados da task */);
+    final tTask = Task(
+        id: '1',
+        title: 'Test Task',
+        description: 'teste',
+        status: TaskStatus.todo,
+        boardName: 'teste');
 
     when(() => mockUseCase(any())).thenAnswer((_) async => {});
 
@@ -14,5 +31,5 @@ void main() {
 
     expect(controller.value.length, 1);
     expect(controller.value.contains(tTask), true);
-  })
+  });
 }

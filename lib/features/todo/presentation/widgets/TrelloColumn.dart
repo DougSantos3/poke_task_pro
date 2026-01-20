ef73db5c8e34159a0724354889e2afd4c7383c2f
Doug Santos <douglasmartinssantos7@gmail.com>
2026-01-20 17:28:38 -0300

    feat:  widget TrelloCollumn

--- lib/features/todo/presentation/widgets/TrelloColumn.dart
+++ lib/features/todo/presentation/widgets/TrelloColumn.dart
@@ -1,9 +1,50 @@
-
-
-
 import 'package:flutter/material.dart';
+import 'package:poke_task_pro/features/todo/domain/entities/task.dart';
+import 'package:poke_task_pro/features/todo/domain/entities/task_status.dart';
+import 'package:poke_task_pro/features/todo/presentation/controllers/todo_controller.dart';
 
 class TrelloColumn extends StatelessWidget {
+  final TaskStatus status;
+  final TodoController controller;
 
-  
+  const TrelloColumn(
+      {super.key, required this.status, required this.controller});
+
+      @override
+      Widget build(BuildContext context) {
+        //para as colunas aceitarem a tarefa
+        return DragTarget<Task>(
+          onAccept: (task) {
+            // Lógica de mudança de status ao soltar, 
+            //A coluna fica "vigiando" se alguma Task vai cair nela.
+            controller.updateTaskStatus(task, status);
+          },
+            builder: (context, candidateData, rejectData) {
+              final tasks = controller.getTaskBtStatus(status);
+              return Container(
+                width: 300, 
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Color(status.colorValue).withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Text(status.label, style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      )),
+                    ),
+                    Expanded(child: ListView.builder(
+                      itemCount: tasks.length,
+                      itemBuilder: (context, index) => TaskCard(task: tasks[index]),
+                ),
+              ),
+            ]
+          ),
+        );
+      },
+    );
+  }
 }

e647fed84153245ad30bfcc46020cdea8096e48d
Douglas Santos <douglas.santos@opah.com.br>
2026-02-02 15:59:29 -0300

    feat: new task card

---
+++ lib/features/todo/presentation/widgets/TaskCard.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:poke_task_pro/features/todo/domain/entities/task.dart';
+
+class TaskCard extends StatelessWidget {
+  final Task task;
+  const TaskCard({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return LongPressDraggable<Task>(
+      data: task,
+      feedback: Material(
+        // O que aparece quando arrasta
+        // O card só sai do lugar se o usuário segurar por um tempo (igual no Trello mobile).
+        child: Container(
+          width: 280,
+          padding: const EdgeInsets.all(16),
+          color: Colors.white,
+          child: Text(task.title),
+        ),
+      ),
+      childWhenDragging: Opacity(opacity: 0, child: _cardBody()),
+      child: _cardBody(),
+    );
+  }
+
+  Widget _cardBody() {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: ListTile(title: Text(task.title)),
+    );
+  }
+}

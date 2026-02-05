d9f3345250553995e007f70d06f9b78d681e12e6
Douglas Santos <douglas.santos@opah.com.br>
2026-02-05 15:35:40 -0300

    feat: Add `BoardPage` smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,25 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:poke_task_pro/main.dart';
+import 'package:http/http.dart' as http;
+import 'package:poke_task_pro/features/todo/data/datasources/pokemon_remote_datasource.dart';
+import 'package:poke_task_pro/features/todo/data/repositories/todo_repository_impl.dart';
+import 'package:poke_task_pro/features/todo/domain/usecases/add_task.dart';
+import 'package:poke_task_pro/features/todo/presentation/controllers/todo_controller.dart';
+import 'package:poke_task_pro/features/todo/presentation/pages/board_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('BoardPage smoke test', (WidgetTester tester) async {
+    // 1. Initialize dependencies
+    final mockRepo = TodoRepositoryImpl(
+        remoteDataSource: PokemonRemoteDataSourceImpl(client: http.Client()));
+    final addTaskUseCase = AddTask(mockRepo);
+    final controller = TodoController(addTaskUseCase);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // 2. Build BoardPage
+    await tester
+        .pumpWidget(MaterialApp(home: BoardPage(controller: controller)));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // 3. Verify that the title appears
+    expect(find.text('Poké-Task Trello'), findsOneWidget);
   });
 }
